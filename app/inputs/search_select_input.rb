d013073ecea8aadff34d68b85ecbc3ad992b7e8d
Hossam Eldeen Hawary <hossameldeen.hawary@gmail.com>
2021-01-21 14:58:36 +0200

    Accept defaults for the search select input

--- app/inputs/search_select_input.rb
+++ app/inputs/search_select_input.rb
@@ -12,17 +12,18 @@ class SearchSelectInput < ActiveAdminAddons::InputBase
   end
 
   def load_control_attributes
+    opts = select_options
     load_class(@options[:class])
-    load_data_attr(:fields, default: ["name"], formatter: :to_json)
-    load_data_attr(:predicate, default: "contains")
-    load_data_attr(:url, default: url_from_method)
-    load_data_attr(:response_root, default: tableize_method)
-    load_data_attr(:display_name, default: "name")
-    load_data_attr(:minimum_input_length, default: 1)
-    load_data_attr(:width, default: "80%")
-    load_data_attr(:multiple, default: false)
-    load_data_attr(:per_page, default: 30)
-    load_data_attr(:query_extras, default: 'null')
+    load_data_attr(:fields, default: opts[:fields], formatter: :to_json)
+    load_data_attr(:predicate, default: opts[:predicate])
+    load_data_attr(:url, default: opts[:url])
+    load_data_attr(:response_root, default: opts[:response_root])
+    load_data_attr(:display_name, default: opts[:display_name])
+    load_data_attr(:minimum_input_length, default: opts[:minimum_input_length])
+    load_data_attr(:width, default: opts[:width])
+    load_data_attr(:multiple, default: opts[:multiple])
+    load_data_attr(:per_page, default: opts[:per_page])
+    load_data_attr(:query_extras, default: opts[:query_extras])
     load_data_attr(
       :order,
       value: @options[:order_by],
@@ -33,6 +34,16 @@ class SearchSelectInput < ActiveAdminAddons::InputBase
       load_attr(:name, value: method_to_input_array_name)
       load_attr(:multiple, value: "multiple")
     end
+  end
+
+  def select_options
+    default_options = {
+      fields: ["name"],  predicate: "contains", display_name: 'name',
+      minimum_input_length: 1, width: "80%", multiple: false,
+      per_page: 30, query_extras: 'null',
+      url: url_from_method, response_root: tableize_method
+    }
+    default_options.merge(ActiveadminAddons.default_select_options)
   end
 
 end
